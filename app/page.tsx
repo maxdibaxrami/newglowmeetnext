0abf7e65c7143887a3a6a8b41de9394609c714a4
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-10-24 07:13:22 +0300

    m

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,13 @@ import ExplorePage from "../components/explore/index";
 
 import TopBar from "@/components/tobBar";
 import BottomMenu from "@/components/bottomMenu/index";
+
+import { useTheme } from "next-themes";
+
+
 export default function Home() {
   const [selectedTab, setSelectedTab] = useState("");
+  const Theme = useTheme();
 
   const onChangeMenu = (value) => {
     setSelectedTab(value);

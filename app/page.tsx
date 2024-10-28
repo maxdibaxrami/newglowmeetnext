c9f0299642486b88b77f73db5c575d7ab1f25e38
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-10-29 01:46:44 +0300

    m

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import ExplorePage from "../components/explore/index";
 
 import TopBar from "@/components/tobBar";
 import BottomMenu from "@/components/bottomMenu/index";
-
+import { motion, AnimatePresence } from "framer-motion";
 export default function Home() {
   const [selectedTab, setSelectedTab] = useState("");
 
@@ -21,11 +21,64 @@ export default function Home() {
       <TopBar selectedTab={selectedTab} />
 
       <section className="flex flex-col items-center justify-center gap-4 md:py-10 app-background">
-        {selectedTab === "explore" && <ExplorePage />}
-        {selectedTab === "chat" && <ChatPage />}
-        {selectedTab === "profile" && <ProfilePage />}
-        {selectedTab === "back" && <ExplorePage />}
+      {selectedTab === "explore" && <AnimatePresence mode="wait">
+          <motion.div
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.4 }
+              }}            >
+               
+              <ExplorePage />
+          </motion.div>
+        </AnimatePresence>}
+
+        {selectedTab === "chat" && <AnimatePresence mode="wait">
+          <motion.div
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.4 }
+              }}            >
+ <ChatPage />
+          </motion.div>
+        </AnimatePresence>}
+
+
+        {selectedTab === "profile" && <AnimatePresence mode="wait">
+          <motion.div
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.4 }
+              }}            >
+ <ProfilePage />
+
+          </motion.div>
+        </AnimatePresence>}
+
+        {selectedTab === "back" && <AnimatePresence mode="wait">
+          <motion.div
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.4 }
+              }}
+            >
+ <ExplorePage />
+          </motion.div>
+        </AnimatePresence>}
         <BottomMenu onChangeMenu={onChangeMenu} />
+
       </section>
     </main>
   );

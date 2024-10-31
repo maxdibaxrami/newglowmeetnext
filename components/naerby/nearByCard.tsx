46d1d35239c03b1322038f12613ed4f3399864e9
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-10-31 10:59:09 +0300

    m

--- components/naerby/nearByCard.tsx
+++ components/naerby/nearByCard.tsx
@@ -1,24 +1,27 @@
-import { Card, CardFooter, Image, CardBody } from "@nextui-org/react";
+import { Card, CardFooter, Image, CardBody, Badge } from "@nextui-org/react";
 
 const NearByCard = ({ data, onCardClick, num }) => {
   return (
-    <Card
-        isFooterBlurred
-        radius="lg"
-        className="border-none"
-        onPress={() => onCardClick(data)}
-        isPressable
-        >
-        <Image
-          alt="Woman listing to music"
-          className="object-cover"
-          src={data.avatar}
-          style={{height:"100%", width:"100%"}}  
-        />
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <p className="text-tiny text-white/80 text-handller">{`${data.name} ${data.age}`}</p>
-        </CardFooter>
-    </Card>
+    <Badge isInvisible={num%2 === 0? true :false} style={{top:"14%",right:"20%"}} disableOutline variant="shadow" content="New" color="primary" size="sm">
+      <Card
+          isFooterBlurred
+          radius="lg"
+          className="border-none"
+          onPress={() => onCardClick(data)}
+          isPressable
+          >
+          <Image
+            alt="Woman listing to music"
+            className="object-cover"
+            src={data.avatar}
+            style={{height:"100%", width:"100%"}}  
+          />
+          
+          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+            <p className="text-tiny text-white/80 text-handller">{`${data.name} ${data.age}`}</p>
+          </CardFooter>
+      </Card>
+    </Badge>
   );
 };
 

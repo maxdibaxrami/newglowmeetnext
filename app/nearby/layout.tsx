dbafbcca79a67f316e1b7df6ae253fa70f0578b2
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-11-02 23:32:12 +0300

    m

--- app/nearby/layout.tsx
+++ app/nearby/layout.tsx
@@ -4,8 +4,8 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+    <section className="flex items-start flex-col gap-4 py-8 md:py-10">
+      <div className="inline-block w-full max-w-lg text-center">
         {children}
       </div>
     </section>
